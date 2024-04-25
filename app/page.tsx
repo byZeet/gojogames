9273adb3bb6303e92eaf82fdae293f7ea29c8d2d
byZeet <cosminrv98@gmail.com>
2024-04-25 20:06:26 +0200

    refactor: :art: delete-imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import {Checkbox} from "@nextui-org/checkbox";
-import {DateRangePicker} from "@nextui-org/date-picker";
-
 export default function Home() {
   return (
     <main className="w-screen h-screen">
-      <h1>hello world</h1>
-      <Checkbox defaultSelected>Option</Checkbox>
-      <DateRangePicker 
-      label="Stay duration" 
-      className="max-w-xs" 
-    />
+
     </main>
   )
 }
