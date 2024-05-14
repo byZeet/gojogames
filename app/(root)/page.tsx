34b02805a2bc8d94427e02fa0ab7b3a504e548d1
byZeet <cosminrv98@gmail.com>
2024-05-14 12:44:07 +0200

    arreglo rutas

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { Slider } from '@/components/Slider/Slider';
-import  CardGame from '@/components/CardGame/CardGame';
-import { CardCategory } from '@/components/CardCategory/CardCategory';
-import  OfertsGame  from '@/components/Oferts/OfertsGame';
-import { ReviewsGame } from '@/components/Reviews/ReviewsGame';
-import  NextLaunchGame  from '@/components/NextLaunch/NextLaunchGame';
+import { Slider } from '../../components/Slider/Slider'
+import  CardGame from '../../components/CardGame/CardGame'
+import { CardCategory } from '../../components/CardCategory/CardCategory';
+import  OfertsGame  from '../../components/Oferts/OfertsGame'
+import { ReviewsGame } from '../../components/Reviews/ReviewsGame'
+import  NextLaunchGame  from '../../components/NextLaunch/NextLaunchGame'
 import { Betagames } from '../../components/BetaGames/BetaGames';
 
 import React from "react";
